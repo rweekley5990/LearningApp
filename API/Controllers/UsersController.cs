0119f62869a79dee4726a4e23d84b798638750f4
Ryan Weekley <rweekley@varonis.com>
2025-04-15 09:57:48 -0400

    Added Service Extentions for refactor of Program.cs

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,7 @@ namespace API.Controllers;
 
 public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -17,7 +19,7 @@ public class UsersController(DataContext context) : BaseApiController
         return Ok(users);
     }
 
-
+    [Authorize]
     [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
